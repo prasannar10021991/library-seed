522125127c6b0ab862fbac3870a479ada60f02e2
Prasanna Kumar Ravi <prasanna.neos@gmail.com>
2018-06-19 14:53:46 +0530

    Code

---
+++ src/lib/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var lib_component_1 = require("./src/component/lib.component");
+exports.LibComponent = lib_component_1.LibComponent;
+var lib_service_1 = require("./src/service/lib.service");
+exports.LibService = lib_service_1.LibService;
+var module_1 = require("./src/module");
+exports.LibModule = module_1.LibModule;
+//# sourceMappingURL=index.js.map
